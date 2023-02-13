6c7ed42f4263eefb3c2f3a58216e45e8ddb09500
Sangeeth Sajan Baby <sangeethsajanbaby@Sangeeths-MacBook-Air.local>
2023-02-12 20:36:05 -0500

    javaScript tutorial

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,31 @@
 console.log('hello world');
+document.getElementById("metacounter").style.display = "none";
+document.getElementById("fillvalue").style.display = "none";
+var fullname =''
+function onSubmit(){
+    console.log("Entered 1")
+    var fname = document.getElementById('fname').value
+    var lname = document.getElementById('lname').value
+    console.log(fname)
+    if (fname !== "" && lname !== ""){
+        fullname = fname +" "+ lname
+        document.getElementById("fillvalue").style.display = "none";
+        console.log("Entered 2")
+        document.getElementById("warning").style.display = "none";
+        document.getElementById("metacounter").style.display = "block";
+
+    }
+    else{
+        console.log("Entered 3")
+        document.getElementById("fillvalue").style.display = "block";
+        document.getElementById("metacounter").style.display = "none";
+        document.getElementById("warning").style.display = "block";
+
+    }
+}
+
+function counterFunction(){
+    document.getElementById('counter').innerHTML = parseInt(document.getElementById('counter').innerHTML) + 1
+    document.getElementById('name').innerHTML = fullname
+    console.log(counter)
+}
