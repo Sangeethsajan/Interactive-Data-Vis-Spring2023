5d9a9a720a3957e77ac3403c045e03b6f459207e
Sangeeth Sajan Baby <sangeethsajanbaby@Sangeeths-MacBook-Air.local>
2023-02-28 18:43:27 -0500

    2_1 commit

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -4,9 +4,30 @@
 // const height = ;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+d3.csv('addresses.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
+
+
+    
+    const table = d3.select("#container")
+      .append("table")
+      .attr("style","border: 1px solid black")
+      table.append("thead")
+      const tbody =table.append("tbody")
+        // .append("tr")
+        // .attr("class","row")
+      const row = tbody.selectAll("tr")
+        .data(data)
+        .join("tr")
+        .attr("class","student")
+        .attr("id",data=>data.last)
+      row.append("td")
+        .text(data => data.first)
+      row.append("td")
+        .text(data =>data.last)
+
+    console.log(table)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
