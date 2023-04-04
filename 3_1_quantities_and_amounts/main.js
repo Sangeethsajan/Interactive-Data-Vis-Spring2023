e3dca0726232f43e1de7e8820397fe2286b4e160
Sangeeth Sajan Baby <sangeethsajanbaby@sangeeths-air.mynetworksettings.com>
2023-04-03 22:13:52 -0400

    Project Prospectus

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,22 +1,25 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth*0.7,
+  height = window.innerHeight*0.7,
+  margin = {top: 30, right: 30, bottom: 30, left: 150},
+  radius = 10;
 
 // // since we use our scales in multiple functions, they need global scope
 // let xScale, yScale;
+let xScale,yScale
+
 
 /* APPLICATION STATE */
 let state = {
-  // data: [],
+  data: [],
 };
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
+d3.csv('squirrelActivities.csv', d3.autoType).then(raw_data => {
   console.log("data", raw_data);
   // save our data to application state
   state.data = raw_data;
+  // console.log(state.data)
   init();
 });
 
@@ -24,9 +27,18 @@ d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   /* SCALES */
+xScale = d3.scaleBand()
+  .domain([state.data.map(d=>d.activity)])
+  .range([margin.left, width-margin.right])
+  .paddingInner(0.2)
+
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data,d=>d.count)])
+    .range([height-margin.bottom, margin.top])
 
 
-
+  // SVG
+  
   draw(); // calls the draw function
 }
 
@@ -34,7 +46,20 @@ function init() {
 // we call this every time there is an update to the data/state
 function draw() {
   /* HTML ELEMENTS */
- 
+const svg = d3.select('#conatiner')
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height)
+
+const rect = svg
+  .selectAll("rect.bar")
+  .data(state.data)
+  .join("rect")
+  .attr("class","bar")
+  .attr("width",xScale.bandwidth())
+  .attr("x", d=>xScale(d.activity))
+  .attr("y",d=>yScale(d.count))
+  .attr("height",d=>height-yScale(d=>d.count))
 
 
 }
--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,15 +1,15 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth*0.7,
+  height = window.innerHeight*0.7,
+  margin = {top: 30, right: 30, bottom: 30, left: 150},
+  radius = 10;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+let svg;
+let xScale;
+let yScale;
+let colorScale;
 
 /* APPLICATION STATE */
 let state = {
@@ -30,16 +30,38 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   // + SCALES
+xScale = d3.scaleLinear()
+  .domain(d3.extend(state.data, d=> d.ideologyScore2020))
+  .range([margin.left, width-margin.right])
 
+  yScale = d3.scaleLinear()
+    .domain(d3.extend(state.data, d=>d.envScore2020))
+    .range([height-margin.top, margin.bottom])
+
+  colorScale = d3.scaleOrdinal()
+    .domain(["R","D"])
+    .range(["red","d"])
+
+  const filteredData = state.data.filter((d))=> d.
 
   // + AXES
 
 
   // + UI ELEMENT SETUP
+const selectElement = d3.select("#dropdown")
+  .data(["All","Democrat","Republican"])
+  .join('option')
+  .attr("value",d=>d)
+  .html(d=>d)
 
-
+  .on('change',(event)=>{
+    console.log("selected",event.target.value)
+  })
   // + CREATE SVG ELEMENT
-
+  svg = d3.select("#container")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height)
 
   // + CALL AXES
 
