05acdd553619b995ce0dea35d78df8840e342b3e
Sangeeth Sajan Baby <sangeethsajanbaby@Sangeeths-MacBook-Air.local>
2023-05-16 15:01:29 -0400

    Pre Final

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -25,6 +25,14 @@ d3.csv("squirrelActivities.csv", d3.autoType)
     .range([height - margin, margin])
     .paddingInner(.2)
 
+    var tooltipForData = d3
+.select("#container")
+.append("div")
+.attr("id", "tooltip")
+.style("opacity", 0);
+
+
+
     /* HTML ELEMENTS */
 
     // append rectangles 
@@ -34,14 +42,38 @@ d3.csv("squirrelActivities.csv", d3.autoType)
     .attr("class", "bar")
     .attr("x", xScale(0))
     .attr("y", d => yScale(d.activity))
-    .attr("width", d => xScale(d.count)-margin)
+    .attr("width", d => xScale(0))
     .attr("height", yScale.bandwidth())
     .attr("fill", "#69b3a2")
     .attr("text","Hello")
+    .on('mouseover', function(event,d) {
+      d3.select(this).attr('stroke', '#333').attr('stroke-width', 2).attr("fill", "lavender");
+      tooltipForData.transition().duration(100).style("opacity", 0.95);
+      tooltipForData
+        .html(
+          "<span>Year: </span>" +
+          d.activity +
+          "<br>" +
+           "<span>Polio Deaths: </span>" +
+          d.count
+           )
+      })
+      .on('mouseout', function() {
+        d3.select(this).attr('stroke', null).attr("fill", "#69b3a2");
+        tooltipForData.transition().duration(100).style("opacity", 0)
+      })
+
+    svg.selectAll("rect.bar")
+  .transition()
+  .duration(1000)
+  .attr("x", function(d) { return xScale(0); })
+  .attr("width", function(d) { return xScale(d.count)-margin; })
+  .delay(function(d,i){console.log(i) ; return(i*100)})
     /* Axes */
     const xAxis = d3.axisBottom(xScale)
     console.log(xAxis) 
     const yAxis = d3.axisLeft(yScale)
+    
 
     svg
       .append("g")
@@ -79,4 +111,3 @@ svg.append("text")
         .text("Squirrel Activities")
 
   });
-Footer
